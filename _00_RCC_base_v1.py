b4c2b4a81a3cc26ea725fe536d114d77e1f21fe0
Cade Young <82617358+C19130@users.noreply.github.com>
2022-09-18 09:15:46 +0000

    Commented on the different loops and functions throughout the code, describing the name; version and a description of what they do

--- _00_RCC_base_v1.py
+++ _00_RCC_base_v1.py
@@ -1,7 +1,9 @@
+""" Recipe Cost Calculator, Cade Young, 18/09/22, allow the user to calculate the toatal cost of a recipe aswell as cost per serving"""
+
 import os
 import time
 
-
+# Instructions, print the instructions for the user to view for 30 seconds before clearing the output and continuing with the outcome
 def instructions():
   print(">>>>>Instructions<<<<<")
   print("Input the name of your recipe and the serving size.")
@@ -17,6 +19,7 @@ def instructions():
 
 instructions()
 
+# Name Not Blank, version 2, ask user to input the name of their recipe, and provide an error if the input is blank
 def not_blank(question, error_message):
   valid = False
 
@@ -35,10 +38,10 @@ name = not_blank("Name of recipe: ",
 
 
 
-
+# Price Loop, version 2, custom error message to be used throughoput the outcome whenever numbers are involved to provide an error to users when they have inputted a word or a number less than zero when asked to provide a number
 def int_check(question):
 
-  error = "Please Make sure you have entered a number that is more than"
+  error = "Please Make sure you have entered a number that is more than 0"
 
   valid = False
   while not valid:
@@ -63,9 +66,9 @@ def int_check(question):
 
 
 
-#start of the loop
+# Start of the loop
 
-# initialise loop so that it runs at least once
+# Initialise loop so that it runs at least once
 shoplist = []
 pricelist = []
 weightlist = []
@@ -83,6 +86,7 @@ print()
 time.sleep(1)
 os.system('clear')
 
+# Ingredient Loop, version 3, ask user to input their items and item measurements to their recipe list and storing inputs into appropriate lists for later output and calculations
 while ingredient != "":
   
   if ingredient != "":
@@ -111,7 +115,7 @@ print()
 print("This is the list of ingredients needed: ")
 print()
 
-
+# Cost Calculator, version 2, calculate and output the total and per serving costs of the users recipe aswell as a price breakdown of the individual ingredients
 for i in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ,16, 17, 18, 19, 20, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]:
   if i == list_num:
     break
